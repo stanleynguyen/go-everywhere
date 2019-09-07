b7f85fdcaf8c5c48e9d868606591370146f41209
Stanley Nguyen <hung.ngn.the@gmail.com>
2019-09-07 09:30:24 +0800

    Replace frontend JS with Go WASM

--- wasm/main.go
+++ wasm/main.go
@@ -1,16 +1,61 @@
+// This is Go WASM equivalent of main.js file
 package main
 
 import "syscall/js"
 
-func lol(this js.Value, args []js.Value) interface{} {
-	for _, arg := range args {
-		println(arg.String())
+func newWS() js.Value {
+	wsURI := wsURI()
+
+	return js.Global().Get("WebSocket").New(wsURI)
+}
+
+func wsURI() string {
+	loc := js.Global().Get("window").Get("location")
+	wsURI := ""
+	if loc.Get("protocol").String() == "https:" {
+		wsURI = "wss:"
+	} else {
+		wsURI = "ws:"
 	}
-	return nil
+	wsURI += "//" + loc.Get("host").String()
+	wsURI += loc.Get("pathname").String() + "led"
+
+	return wsURI
+}
+
+func newOpenHandler() js.Func {
+	return js.FuncOf(
+		func(this js.Value, args []js.Value) interface{} {
+			println("Connected")
+			return nil
+		})
+}
+
+func newMessageHandler(lightElem js.Value) js.Func {
+	return js.FuncOf(
+		func(this js.Value, args []js.Value) interface{} {
+			event := args[0]
+			lightElem.Set("innerHTML", event.Get("data").String())
+			return nil
+		})
+
+}
+
+func newSendHandler(ws js.Value) js.Func {
+	return js.FuncOf(
+		func(this js.Value, args []js.Value) interface{} {
+			ws.Call("send", "switch")
+			return nil
+		})
 }
 
 func registerCallbacks() {
-	js.Global().Set("lol", js.FuncOf(lol))
+	ws := newWS()
+	lightElem := js.Global().Get("document").Call("getElementById", "light")
+
+	ws.Call("addEventListener", "open", newOpenHandler())
+	ws.Call("addEventListener", "message", newMessageHandler(lightElem))
+	js.Global().Set("send", newSendHandler(ws))
 }
 
 func main() {
